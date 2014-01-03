5872bb3752846b79583d609f0adeff47bac00a9a
park sung chan <dnrmrgks@naver.com>
2014-01-03 19:28:47 +0900

    델리게이트
    델리게이 다수의텍스트필

--- TextFieldDeligate/ViewController.m
+++ TextFieldDeligate/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -17,7 +17,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(20, 205, 0, 0)];
+    label.text = @"Z입력금지";
+    label.textColor =[UIColor blackColor];
+    label.backgroundColor = [UIColor clearColor];
+    [label sizeToFit];
+    [self.view addSubview:label];
+    
+    UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(130, 200, 171, 31)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self;
+    textField.tag = 3;
+    
+    [self.view addSubview:textField];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField*)textField{
+    if(2 == textField.tag)
+        return NO;
+    return YES;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField{
+    NSLog(@"INPUT : %@",textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(BOOL)textField:(UITextField*)textField shouldChangeCharactersInRange:(NSRange)range replacementString:
+(NSString *)string{
+    if(3 == textField.tag && NSOrderedSame ==[string compare:
+                                            
+        @"z" options:NSCaseInsensitiveSearch])
+        return NO;
+        return YES;
 }
 
 - (void)didReceiveMemoryWarning
